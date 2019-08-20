23a61f5cc5f4c5e0285aca0b009f874752e50329
Kazuo Tsubaki <kazz12211@gmail.com>
2019-08-20 10:42:01 +0900

    add mail sender

---
+++ send_mail.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = 'aafb7882e21bdf'
+    password = '2354f28b9582f4'
+    message = f"<h3>New Feedback Submitted</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'email1@example.com'
+    receiver_email = 'email2@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'New Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
